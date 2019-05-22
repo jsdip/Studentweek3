ebb51bf003b7a90d20364f9c17e91e5d7488e037
jsdip <kaur5940@sheridancollege.ca>
2019-05-22 09:34:47 -0400

    new repo for students

---
+++ src/wk3/Studentweek3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wk3;
+
+/**
+ *
+ * @author User
+ */
+public class Studentweek3 {
+    public static void main(String[] args){
+        Wk3 s1= new Wk3();
+        s1 .setFirstname("john");
+        s1 .setID(4);
+        
+    }
+    
+}
---
+++ src/wk3/Wk3.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wk3;
+
+/**
+ *
+ * @author User
+ */
+public class Wk3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+        private int ID;
+        private String firstname;
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return the firstname
+     */
+    
+    }
+
+    /**
+     * @return the ID
+  
+        
+    }
+    
+
